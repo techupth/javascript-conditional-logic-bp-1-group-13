01183652510ce086f83e88dfe67627ddaa19017e
GaiKT <124340777+GaiKT@users.noreply.github.com>
2023-09-15 14:53:12 +0700

    fix(ex1-7): send assignment by gai

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,19 @@
 function calculateStudentGrade(score) {
-  // Start coding here
+  if(score <= 59){
+    return "F"
+  }
+  else if(score <= 69){
+    return "D"
+  }
+  else if(score <= 79){
+    return "C"
+  }
+  else if(score <= 89){
+    return "B"
+  }
+  else{
+  return "A"
+  }
 }
 
 // Example case
